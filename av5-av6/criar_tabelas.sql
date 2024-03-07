52935e265ac7320e2e5285d956ae6854cc3d2c82
Jessyca Ferreira <ferreiradasilvajessyca@gmail.com>
2024-03-07 20:25:55 -0300

    fix: use OR conventions

--- av5-av6/criar_tabelas.sql
+++ av5-av6/criar_tabelas.sql
@@ -20,13 +20,40 @@ CREATE OR REPLACE TYPE tp_dados_bancarios AS OBJECT (
 );
 /
 
+
+CREATE OR REPLACE TYPE tp_autor AS OBJECT (
+    autor VARCHAR2(255)
+);
+/
+
+CREATE OR REPLACE TYPE tp_subgenero AS OBJECT (
+    subgenero VARCHAR2(255)
+);
+/
+
 CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
-    cpf VARCHAR2(14),
     telefone VARCHAR2(20)
 );
 /
 
+CREATE OR REPLACE TYPE tp_copia AS OBJECT (
+    numero_da_copia NUMBER
+);
+/
+
+ALTER TYPE tp_copia ADD ATTRIBUTE status_de_disponibilidade VARCHAR2(20) CASCADE;
+/
+
 CREATE OR REPLACE TYPE tp_telefones AS VARRAY(5) OF tp_telefone;
+/
+
+CREATE OR REPLACE TYPE tp_autores AS VARRAY(5) OF tp_autor;
+/
+
+CREATE OR REPLACE TYPE tp_subgeneros AS VARRAY(5) OF tp_subgenero;
+/
+
+CREATE OR REPLACE TYPE tp_copias AS VARRAY(50) OF tp_copia;
 /
 
 CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
@@ -35,7 +62,7 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
     email VARCHAR2(100),
     sexo CHAR(1),
     data_de_nascimento DATE,
-    cep VARCHAR2(8),
+    endereco REF tp_moradia,
     numero NUMBER,
     telefone tp_telefones,
 
@@ -47,7 +74,6 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 /
 
 CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
-    cpf_func VARCHAR2(14),
     ctps VARCHAR2(20),
     conta REF tp_dados_bancarios,
     salario DECIMAL(10,2),
@@ -68,7 +94,6 @@ CREATE TYPE tp_nt_cartoes AS TABLE OF tp_cartao;
 /
 
 CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa (
-    cpf_cliente VARCHAR2(14),
     numero_de_livro_emprestado NUMBER,
     lista_cartoes tp_nt_cartoes
 );
@@ -82,46 +107,27 @@ CREATE OR REPLACE TYPE tp_obra AS OBJECT (
     ano_de_publicacao NUMBER,
     numero_de_paginas NUMBER,
     editora VARCHAR2(100),
+    lista_autores tp_autores,
+    lista_subgeneros tp_subgeneros,
+    lista_copias tp_copias,
 
     ORDER MEMBER FUNCTION comparaNumeroPaginas (X tp_obra) RETURN INTEGER
 );
 /
 
-CREATE OR REPLACE TYPE tp_copia AS OBJECT (
-    isbn VARCHAR2(13),
-    numero_da_copia NUMBER,
-    status_de_disponibilidade VARCHAR2(20)
-);
-/
-
-CREATE OR REPLACE TYPE tp_autor AS OBJECT (
-    isbn VARCHAR2(13)
-);
-/
-
-ALTER TYPE tp_autor ADD ATTRIBUTE nome_autor VARCHAR2(255) CASCADE;
-/
-
-CREATE OR REPLACE TYPE tp_subgenero AS OBJECT (
-    isbn VARCHAR2(13),
-    tipo_subgenero VARCHAR2(255)
-);
-/
-
 CREATE OR REPLACE TYPE tp_livro_emprestado AS OBJECT (
-    cpf_cliente VARCHAR2(14),
+    cliente REF tp_cliente,
     livro_emprestado NUMBER
 );
 /
 
 CREATE OR REPLACE TYPE tp_empresta AS OBJECT (
-    isbn VARCHAR2(13),
-    numero_da_copia NUMBER,
-    data_de_emprestimo DATE,
+    obra REF tp_obra,
+    data_de_emprestimo TIMESTAMP,
     valor_multa NUMBER,
-    cpf_cliente VARCHAR2(14),
-    cpf_func VARCHAR2(14),
-    cpf_func_multa VARCHAR2(14)
+    cliente REF tp_cliente,
+    vendedor REF tp_funcionario,
+    multador REF tp_funcionario
 );
 /
 
@@ -153,16 +159,6 @@ CREATE OR REPLACE TYPE BODY tp_moradia AS
         DBMS_OUTPUT.PUT_LINE('Cidade: ' || SELF.tp_moradia.cidade);
         DBMS_OUTPUT.PUT_LINE('Estado: ' || SELF.enderecotp_moradia.estado);
     END;
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_dados_bancarios AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_cartao AS
-    -- Member procedures and functions here
 END;
 /
 
@@ -200,22 +196,9 @@ END;
 
 CREATE OR REPLACE TYPE BODY tp_funcionario AS
     OVERRIDING MEMBER FUNCTION exibir_nome RETURN VARCHAR2 IS
-        v_supervisor_nome VARCHAR2(50);
     BEGIN
-        SELECT p.nome INTO v_supervisor_nome
-        FROM tb_pessoa p
-        WHERE p.cpf = VALUE(SELF.tp_funcionario.cpf_supervisor).cpf;
-
-        RETURN 'Nome do Funcionário: ' || self.nome || ', Detalhes de seu supervisor: ' || v_supervisor_nome;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RETURN 'Nome do Funcionário: ' || self.nome || ', Supervisor não encontrado';
+        RETURN 'Nome do Funcionário: ';
     END exibir_nome;
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_cliente AS
-    -- Member procedures and functions here
 END;
 /
 
@@ -229,112 +212,48 @@ CREATE OR REPLACE TYPE BODY tp_obra AS
 END;
 /
 
-CREATE OR REPLACE TYPE BODY tp_copia AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_autor AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_subgenero AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_livro_emprestado AS
-    -- Member procedures and functions here
-END;
-/
-
-CREATE OR REPLACE TYPE BODY tp_empresta AS
-    -- Member procedures and functions here
-END;
-/
-
 -- Create tables
 CREATE TABLE tb_moradia OF tp_moradia (
-    CONSTRAINT moradia_pk PRIMARY KEY (cep)
+    cep PRIMARY KEY
 );
 /
 
 CREATE TABLE tb_dados_bancarios OF tp_dados_bancarios (
-    CONSTRAINT dados_bancarios_pk PRIMARY KEY (conta)
+    conta PRIMARY KEY
 );
 /
 
 CREATE TABLE tb_cartao OF tp_cartao (
-    CONSTRAINT cartao_pk PRIMARY KEY (numero_do_cartao)
-);
-/
-
-CREATE TABLE tb_pessoa OF tp_pessoa (
-    CONSTRAINT pessoa_pk PRIMARY KEY (cpf),
-    CONSTRAINT pessoa_cep_fk FOREIGN KEY (cep) REFERENCES tb_moradia (cep)
+    numero_do_cartao PRIMARY KEY
 );
 /
 
 CREATE TABLE tb_funcionario OF tp_funcionario (
-    CONSTRAINT funcionario_pk PRIMARY KEY (cpf_func),
-    CONSTRAINT funcionario_cpf_fk FOREIGN KEY (cpf_func) REFERENCES tb_pessoa (cpf),
+    cpf PRIMARY KEY,
     conta SCOPE IS tb_dados_bancarios,
     cpf_supervisor WITH ROWID REFERENCES tb_funcionario
 );
 /
 
 CREATE TABLE tb_cliente OF tp_cliente (
-    CONSTRAINT cliente_pk PRIMARY KEY (cpf_cliente),
-    CONSTRAINT cliente_cpf_fk FOREIGN KEY (cpf_cliente) REFERENCES tb_pessoa (cpf)
+    cpf PRIMARY KEY
 ) NESTED TABLE lista_cartoes STORE AS nt_cartoes;
 /
 
 CREATE TABLE tb_obra OF tp_obra (
-    CONSTRAINT obra_pk PRIMARY KEY (isbn)
-);
-/
-
-CREATE TABLE tb_copia OF tp_copia (
-    CONSTRAINT copia_pk PRIMARY KEY (isbn, numero_da_copia),
-    CONSTRAINT copia_isbn_fk FOREIGN KEY (isbn) REFERENCES tb_obra (isbn)
-);
-/
-
-CREATE TABLE tb_autor OF tp_autor (
-    CONSTRAINT autor_pk PRIMARY KEY (isbn, nome_autor),
-    CONSTRAINT autor_isbn_fk FOREIGN KEY (isbn) REFERENCES tb_obra (isbn)
-);
-/
-
-CREATE TABLE tb_subgenero OF tp_subgenero (
-    CONSTRAINT subgenero_pk PRIMARY KEY (isbn, tipo_subgenero),
-    CONSTRAINT subgenero_isbn_fk FOREIGN KEY (isbn) REFERENCES tb_obra (isbn)
-);
-/
-
-CREATE TABLE tb_telefone OF tp_telefone (
-    CONSTRAINT telefone_pk PRIMARY KEY (cpf, telefone),
-    CONSTRAINT telefone_cpf_fk FOREIGN KEY (cpf) REFERENCES tb_pessoa (cpf)
+    isbn PRIMARY KEY
 );
 /
 
 CREATE TABLE tb_livro_emprestado OF tp_livro_emprestado (
-    CONSTRAINT livro_emprestado_pk PRIMARY KEY (cpf_cliente, livro_emprestado),
-    CONSTRAINT livro_emprestado_cpf_fk FOREIGN KEY (cpf_cliente) REFERENCES tb_cliente (cpf_cliente)
+    cliente WITH ROWID REFERENCES tb_cliente
 );
 /
 
 CREATE TABLE tb_empresta OF tp_empresta (
-    CONSTRAINT empresta_pk PRIMARY KEY (cpf_cliente, cpf_func, isbn, numero_da_copia),
-    CONSTRAINT empresta_isbn_fk FOREIGN KEY (isbn, numero_da_copia) REFERENCES tb_copia (isbn, numero_da_copia),
-    CONSTRAINT empresta_cpf_cliente_fk FOREIGN KEY (cpf_cliente) REFERENCES tb_cliente (cpf_cliente),
-    CONSTRAINT empresta_cpf_func_fk FOREIGN KEY (cpf_func) REFERENCES tb_funcionario (cpf_func),
-    CONSTRAINT empresta_cpf_func_multa_fk FOREIGN KEY (cpf_func_multa) REFERENCES tb_funcionario (cpf_func)
+    obra WITH ROWID REFERENCES tb_obra,
+    cliente WITH ROWID REFERENCES tb_cliente,
+    vendedor WITH ROWID REFERENCES tb_funcionario,
+    multador WITH ROWID REFERENCES tb_funcionario
 );
 /
--- av5-av6/povoamento.sql
+++ av5-av6/povoamento.sql
@@ -4,163 +4,35 @@ SET
 
 -- Obra
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('9783161484100', 'Dom Casmurro', 'Livro', 'Classico', 1899, 400, 'Companhia de Letras');
+    ('9783161484100', 'Dom Casmurro', 'Livro', 'Classico', 1899, 400, 'Companhia de Letras', tp_autores(tp_autor('Machado de Assis')), tp_subgeneros(tp_subgenero('Romance')), tp_copias(tp_copia(2, 'Disponível'), tp_copia(5, 'Indisponível'), tp_copia(10, 'Disponível')));
 
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('1233161485610', 'Percy Jackson e o Ladrão de Raios', 'Livro', 'Fantasia', 2004, 180, 'Penguin Random House');
+    ('1233161485610', 'Percy Jackson e o Ladrão de Raios', 'Livro', 'Fantasia', 2004, 180, 'Penguin Random House', tp_autores(tp_autor('Rick Riordan')), tp_subgeneros(tp_subgenero('Infanto-juvenil')), tp_copias(tp_copia(4, 'Disponível'), tp_copia(2, 'Indisponível'), tp_copia(1, 'Disponível')));
 
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('2435161484111', 'Capricho', 'Revista', 'Infanto-juvenil', 2013, 40, 'Capricho');
+    ('2435161484111', 'Capricho', 'Revista', 'Infanto-juvenil', 2013, 40, 'Capricho', tp_autores(tp_autor('Bianca Capricho'), tp_autor('Camila Capricho')), tp_subgeneros(tp_subgenero('Romance')), tp_copias(tp_copia(4, 'Disponível'), tp_copia(2, 'Indisponível'), tp_copia(1, 'Disponível')));
 
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('2452261118100', 'SuperInteressante', 'Revista', 'Curiosidades', 2011, 70, 'Abril');
+    ('2452261118100', 'SuperInteressante', 'Revista', 'Curiosidades', 2011, 70, 'Abril', tp_autores(tp_autor('Pessoa Cientista')), tp_subgeneros(tp_subgenero('Ciência')), tp_copias(tp_copia(4, 'Disponível'), tp_copia(2, 'Indisponível'), tp_copia(1, 'Disponível')));
 
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('8471118484671', 'Homem-Aranha #1', 'Comic', 'Infanto-juvenil', 1967, 40, 'Marvel');
+    ('8471118484671', 'Homem-Aranha #1', 'Comic', 'Infanto-juvenil', 1967, 40, 'Marvel', tp_autores(tp_autor('Stan Lee')), tp_subgeneros(tp_subgenero('Aventura')), tp_copias(tp_copia(4, 'Disponível'), tp_copia(2, 'Indisponível'), tp_copia(1, 'Disponível')));
 
 INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
+    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora, lista_autores, lista_subgeneros, lista_copias)
 VALUES
-    ('3452261118311', 'Artifícios das Trevas', 'Livro', 'Fantasia', 2016, 70, 'Galera');
-
-INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
-VALUES
-    ('983515482112', 'Guerra e Paz', 'Livro', 'Clássico', 1848, 600, 'CIA das Letras');
-
-INSERT INTO
-    tb_obra (isbn, nome, tipo, genero, ano_de_publicacao, numero_de_paginas, editora)
-VALUES
-    ('7452261118900', 'Naruto', 'Manga', 'Infanto-juvenil', 2009, 70, 'Abril');
-
--- Copia
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('9783161484100', 1, 'Disponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('9783161484100', 2, 'Indisponivel');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('2435161484111', 1, 'Indisponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('7452261118900', 1, 'Disponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('7452261118900', 2, 'Disponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('3452261118311', 3, 'Disponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('983515482112', 1, 'Indisponível');
-
-INSERT INTO
-    tb_copia (isbn, numero_da_copia, status_de_disponibilidade)
-VALUES
-    ('7452261118900', 4, 'Indisponível');
-
--- Autor
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('9783161484100', 'Machado de Assis');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('1233161485610', 'Rick Riordan');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('2435161484111', 'Camilla Capricho');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('2435161484111', 'Beatriz Capricho');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('8471118484671', 'Stan Lee');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('3452261118311', 'Cassandra Clare');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('983515482112', 'Leo Tolstoy');
-
-INSERT INTO
-    tb_autor (isbn, nome_autor)
-VALUES
-    ('7452261118900', 'Masashi Kishimoto');
-
--- Subgenero
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('9783161484100', 'Romance');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('1233161485610', 'Infanto-juvenil');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('1233161485610', 'Mitologia Grega');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('8471118484671', 'Fantasia');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('3452261118311', 'Romance');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('983515482112', 'Romance');
-
-INSERT INTO
-    tb_subgenero (isbn, tipo_subgenero)
-VALUES
-    ('7452261118900', 'Infanto-juvenil');
-
+    ('3452261118311', 'Artifícios das Trevas', 'Livro', 'Fantasia', 2016, 70, 'Galera', tp_autores(tp_autor('Cassandra Clare')), tp_subgeneros(tp_subgenero('Romance'), tp_subgenero('Jovem Adulto')), tp_copias(tp_copia(4, 'Disponível'), tp_copia(2, 'Indisponível'), tp_copia(1, 'Disponível')));
+    
 -- Moradia
 INSERT INTO
     tb_moradia (cep, rua, bairro, cidade, estado)
@@ -271,221 +143,144 @@ VALUES
     ('8901234598765432', '456', '02/2024');
 
 -- Funcionario
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('987.654.321-00', '98765432102', '00001234561234567', 4200.80, NULL);
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('321.098.765-67', '34567890108', '00007293817293817', 1320.00, NULL);
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('123.456.789-09', '12345678901', '00000876224123688', 1000.00, '987.654.321-00');
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('234.567.890-12', '45678901203', '00002468012468012', 1400.22, '987.654.321-00');
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('345.678.901-23', '89012345604', '00003698723698723', 1984.32, '987.654.321-00');
-INSERT INTO
-    tb_funcionario (cpf_func, ctps, conta, salario, cpf_supervisor)
-VALUES
-    ('876.543.210-34', '23456789005', '00004000014000014', 2200.99, '321.098.765-67');
--- Funcionario
 INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('987.654.321-00', '98765432102', REF(tp_dados_bancarios('00001234561234567', '05005', '86420973')), 4200.80, NULL)
+    tp_funcionario(
+        '987.654.321-00',
+        'João Silva',
+        'joao@example.com',
+        'M',
+        DATE '1999-01-15',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '01234567'),
+    	1,
+    	tp_telefones(tp_telefone('5555555555'), tp_telefone('6666666666')),
+        '98765432102',
+        (SELECT REF(x) FROM tb_dados_bancarios x WHERE x.conta = '00001234561234567'),
+        5200.80,
+        NULL
+    )
 );
+
 INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('321.098.765-67', '34567890108', REF(tp_dados_bancarios('00007293817293817', '08008', '25814736')), 1320.00, NULL)
+    tp_funcionario(
+        '321.098.765-67',
+        'Maria Silva',
+        'maria@example.com',
+        'F',
+        DATE '1995-05-15',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '54321098'),
+    	10,
+    	tp_telefones(tp_telefone('5559555555'), tp_telefone('1666666666')),
+        '98765432132',
+        (SELECT REF(x) FROM tb_dados_bancarios x WHERE x.conta = '00002468012468012'),
+        4200.80,
+        (SELECT REF(x) FROM tb_funcionario x WHERE x.cpf = '987.654.321-00')
+    )
 );
+
 INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('123.456.789-09', '12345678901', REF(tp_dados_bancarios('00000876224123688', '06006', '74125896')), 1000.00, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '987.654.321-00'))
+    tp_funcionario(
+        '123.456.789-09',
+        'Ana Clarisse',
+        'anac@example.com',
+        'F',
+        DATE '2000-05-20',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '45678901'),
+    	24,
+    	tp_telefones(tp_telefone('5529555555'), tp_telefone('1666666696')),
+        '12345678901',
+        (SELECT REF(x) FROM tb_dados_bancarios x WHERE x.conta = '00000876224123688'),
+        1290.80,
+        (SELECT REF(x) FROM tb_funcionario x WHERE x.cpf = '987.654.321-00')
+    )
 );
+
 INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('234.567.890-12', '45678901203', REF(tp_dados_bancarios('00002468012468012', '07007', '36985214')), 1400.22, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '987.654.321-00'))
-);
-INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('345.678.901-23', '89012345604', REF(tp_dados_bancarios('00003698723698723', '07007', '36985214')), 1984.32, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '987.654.321-00'))
-);
-INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('876.543.210-34', '23456789005', REF(tp_dados_bancarios('00004000014000014', '05005', '86420973')), 2200.99, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '321.098.765-67'))
-);
-INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('987.654.321-45', '59876543206', REF(tp_dados_bancarios('00005432165432165', '06006', '74125896')), 3290.00, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '321.098.765-67'))
-);
-INSERT INTO tb_funcionario VALUES (
-    tp_funcionario('567.890.123-56', '01234567807', REF(tp_dados_bancarios('00006876546876546', '07007', '36985214')), 2209.00, (SELECT REF(f) FROM tb_funcionario f WHERE f.cpf_func = '321.098.765-67'))
+    tp_funcionario(
+        '234.567.890-12',
+        'Mário Sena',
+        'mario@example.com',
+        'M',
+        DATE '2002-12-20',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '45678901'),
+    	30,
+    	tp_telefones(tp_telefone('5529555955'), tp_telefone('1696666696')),
+        '45678901203',
+        (SELECT REF(x) FROM tb_dados_bancarios x WHERE x.conta = '00004000014000014'),
+        1984.80,
+        NULL
+    )
 );
 
 -- Cliente
 INSERT INTO tb_cliente VALUES (
-    tp_cliente('111.222.333-44', 45, tp_nt_cartoes(tp_cartao('1234598765432456', '123', '12/2025')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('444.555.666-77', 89, tp_nt_cartoes(tp_cartao('6543210987654321', '456', '06/2023')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('222.333.444-55', 23, tp_nt_cartoes(tp_cartao('9876543210987654', '789', '09/2024')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('999.888.777-66', 45, tp_nt_cartoes(tp_cartao('2345987654324567', '234', '03/2022')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('777.888.999-00', 67, tp_nt_cartoes(tp_cartao('3210987654321098', '567', '08/2026')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('666.777.888-99', 89, tp_nt_cartoes(tp_cartao('5432109876543210', '123', '11/2025')))
-);
-INSERT INTO tb_cliente VALUES (
-    tp_cliente('333.444.555-66', 23, tp_nt_cartoes(tp_cartao('8901234598765432', '456', '02/2024')))
+    tp_cliente(
+        '111.222.333-44',
+        'Mário Silva',
+        'marios@example.com',
+        'M',
+        DATE '2002-12-20',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '45678901'),
+    	45,
+    	tp_telefones(tp_telefone('5529555905'), tp_telefone('1696660696')),
+        2,
+        tp_nt_cartoes(tp_cartao('1234598765432456', '123', '12/2025'))
+    )
 );
 
--- Telefone
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('123.456.789-09', '1122334455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('123.456.789-09', '8812332455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('987.654.321-00', '5544332211')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('234.567.890-12', '9988776655')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('345.678.901-23', '6677889900')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('876.543.210-34', '1122334455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('876.543.210-34', '9922531455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('987.654.321-45', '9988776655')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('567.890.123-56', '6677889900')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('321.098.765-67', '5544332211')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('111.222.333-44', '1122334455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('444.555.666-77', '6677889900')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('222.333.444-55', '5544332211')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('999.888.777-66', '9988776655')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('777.888.999-00', '6677889900')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('666.777.888-99', '1122334455')
-);
-INSERT INTO tb_telefone VALUES (
-    tp_telefone('333.444.555-66', '9988776655')
+INSERT INTO tb_cliente VALUES (
+    tp_cliente(
+        '222.333.444-55',
+        'Isabela Flores',
+        'isabela@example.com',
+        'F',
+        DATE '1980-12-02',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '54321098'),
+    	12,
+    	tp_telefones(tp_telefone('5529551905'), tp_telefone('1696160696')),
+        3,
+        tp_nt_cartoes(tp_cartao('6543210987654321', '456', '06/2023'))
+    )
 );
 
--- Obra
-INSERT INTO tb_obra VALUES (
-    tp_obra('9783161484100', 'Nome Obra 1', 'Tipo Obra 1', 'Genero Obra 1', 2022, 300, 'Editora Obra 1')
-);
-INSERT INTO tb_obra VALUES (
-    tp_obra('2435161484111', 'Nome Obra 2', 'Tipo Obra 2', 'Genero Obra 2', 2023, 400, 'Editora Obra 2')
-);
-INSERT INTO tb_obra VALUES (
-    tp_obra('3452261118311', 'Nome Obra 3', 'Tipo Obra 3', 'Genero Obra 3', 2024, 500, 'Editora Obra 3')
-);
-
--- Copia
-INSERT INTO tb_copia VALUES (
-    tp_copia('9783161484100', 1, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('9783161484100', 2, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('9783161484100', 3, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('2435161484111', 1, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('2435161484111', 2, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('3452261118311', 1, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('3452261118311', 2, 'Disponível')
-);
-INSERT INTO tb_copia VALUES (
-    tp_copia('3452261118311', 3, 'Disponível')
-);
-
--- Autor
-INSERT INTO tb_autor VALUES (
-    tp_autor('9783161484100', 'Nome Autor 1')
-);
-INSERT INTO tb_autor VALUES (
-    tp_autor('2435161484111', 'Nome Autor 2')
-);
-INSERT INTO tb_autor VALUES (
-    tp_autor('3452261118311', 'Nome Autor 3')
-);
-
--- Subgenero
-INSERT INTO tb_subgenero VALUES (
-    tp_subgenero('9783161484100', 'Subgenero 1')
-);
-INSERT INTO tb_subgenero VALUES (
-    tp_subgenero('2435161484111', 'Subgenero 2')
-);
-INSERT INTO tb_subgenero VALUES (
-    tp_subgenero('3452261118311', 'Subgenero 3')
+INSERT INTO tb_cliente VALUES (
+    tp_cliente(
+        '999.888.777-66',
+        'Gabriel Flores',
+        'gabriel@example.com',
+        'M',
+        DATE '1988-11-01',
+        (SELECT REF(x) FROM tb_moradia x WHERE x.cep = '54321098'),
+    	2,
+    	tp_telefones(tp_telefone('5529551915'), tp_telefone('1696160616')),
+        10,
+        tp_nt_cartoes(tp_cartao('2345987654324567', '234', '03/2022'))
+    )
 );
 
 -- Livro Emprestado
 INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('111.222.333-44', 4)
+    tp_livro_emprestado((SELECT REF(x) FROM tb_cliente x WHERE x.cpf = '999.888.777-66'), 4)
 );
+
 INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('444.555.666-77', 8)
+    tp_livro_emprestado((SELECT REF(x) FROM tb_cliente x WHERE x.cpf = '222.333.444-55'), 8)
 );
+
 INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('222.333.444-55', 2)
-);
-INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('999.888.777-66', 4)
-);
-INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('777.888.999-00', 6)
-);
-INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('666.777.888-99', 8)
-);
-INSERT INTO tb_livro_emprestado VALUES (
-    tp_livro_emprestado('333.444.555-66', 2)
+    tp_livro_emprestado((SELECT REF(x) FROM tb_cliente x WHERE x.cpf = '111.222.333-44'), 2)
 );
 
 -- Empresta
+
 INSERT INTO tb_empresta VALUES (
-    tp_empresta('9783161484100', 1, '2023-12-18', 0, '111.222.333-44', '123.456.789-09', NULL)
-);
-INSERT INTO tb_empresta VALUES (
-    tp_empresta('2435161484111', 1, '2023-12-21', 0, '222.333.444-55', '876.543.210-34', NULL)
-);
-INSERT INTO tb_empresta VALUES (
-    tp_empresta('3452261118311', 3, '2023-12-24', 0, '666.777.888-99', '321.098.765-67', NULL)
+    tp_empresta(
+        (SELECT REF(x)
+         FROM tb_obra x, TABLE(x.lista_copias) c
+         WHERE x.isbn = '9783161484100' AND c.status_de_disponibilidade = 'Disponível' AND c.numero_da_copia = 2),
+        CURRENT_TIMESTAMP,
+        0,
+        (SELECT REF(x) FROM tb_cliente x WHERE x.cpf = '111.222.333-44'),
+        (SELECT REF(x) FROM tb_funcionario x WHERE x.cpf = '123.456.789-09'),
+        NULL
+    )
 );
